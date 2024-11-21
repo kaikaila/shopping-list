5b7e7b53a9e551c1a6d6333a8a940c2146a53101
kaikaila <lyk2772@126.com>
2024-11-21 14:35:17 -0800

    add list item is done

--- script.js
+++ script.js
@@ -1,208 +1,46 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
-const clearBtn = document.getElementById('clear');
-const itemFilter = document.getElementById('filter');
-const formBtn = itemForm.querySelector('button');
-let isEditMode = false;
+// 以下变量定义在外部，使得所有method都能用
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
 
-function displayItems() {
-  const itemsFromStorage = getItemsFromStorage();
-  itemsFromStorage.forEach((item) => addItemToDOM(item));
-  checkUI();
-}
-
-function onAddItemSubmit(e) {
+function addItem(e) {
+  // 老师说这句话是防止触发事件e自身的default行为，也即submit to file
   e.preventDefault();
-
-  // trim the input value to remove whitespace - disallowing duplicate items due to white space in the process
-  const newItem = itemInput.value.trim();
-
-  // Validate Input
-  if (newItem === '') {
-    alert('Please add an item');
+  newItem = itemInput.value;
+  // validate input
+  if (newItem === "") {
+    alert("Please add an item");
     return;
   }
+  // for test
+  // console.log("success");
 
-  // Check for edit mode
-  if (isEditMode) {
-    const itemToEdit = itemList.querySelector('.edit-mode');
-
-    removeItemFromStorage(itemToEdit.textContent);
-    itemToEdit.classList.remove('edit-mode');
-    itemToEdit.remove();
-    isEditMode = false;
-  } else {
-    if (checkIfItemExists(newItem)) {
-      alert(`The item "${newItem}" already exists!`);
-      return;
-    }
-  }
-
-  // Create item DOM element
-  addItemToDOM(newItem);
-
-  // Add item to local storage
-  addItemToStorage(newItem);
-
-  checkUI();
-
-  itemInput.value = '';
-}
-
-function addItemToDOM(item) {
   // Create list item
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(item));
-
-  const button = createButton('remove-item btn-link text-red');
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
-
-  // Add li to the DOM
   itemList.appendChild(li);
+  // to clear the input box
+  itemInput.value = "";
+  // for testing
+  // console.log(li);
 }
 
 function createButton(classes) {
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.className = classes;
-  const icon = createIcon('fa-solid fa-xmark');
-  button.appendChild(icon);
+  button.appendChild(createIcon("fa-solid fa-xmark"));
   return button;
 }
 
 function createIcon(classes) {
-  const icon = document.createElement('i');
+  const icon = document.createElement("i");
   icon.className = classes;
   return icon;
 }
 
-function addItemToStorage(item) {
-  const itemsFromStorage = getItemsFromStorage();
+// // event listeners
+// 用于向元素添加事件监听器：当遇到submit事件，触发addItem method。submit是预定义的事件
 
-  // Add new item to array
-  itemsFromStorage.push(item);
-
-  // Convert to JSON string and set to local storage
-  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
-}
-
-function getItemsFromStorage() {
-  let itemsFromStorage;
-
-  if (localStorage.getItem('items') === null) {
-    itemsFromStorage = [];
-  } else {
-    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
-  }
-
-  return itemsFromStorage;
-}
-
-function onClickItem(e) {
-  if (e.target.parentElement.classList.contains('remove-item')) {
-    removeItem(e.target.parentElement.parentElement);
-  } else if (e.target.closest('li')) {
-    setItemToEdit(e.target);
-  }
-}
-
-function checkIfItemExists(item) {
-  const itemsFromStorage = getItemsFromStorage();
-  return itemsFromStorage.includes(item);
-}
-
-function setItemToEdit(item) {
-  isEditMode = true;
-
-  itemList
-    .querySelectorAll('li')
-    .forEach((i) => i.classList.remove('edit-mode'));
-
-  item.classList.add('edit-mode');
-  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>   Update Item';
-  formBtn.style.backgroundColor = '#228B22';
-  itemInput.value = item.textContent;
-}
-
-function removeItem(item) {
-  if (
-    confirm(`Are you sure you want to remove the item "${item.textContent}"?`)
-  ) {
-    // Remove item from DOM
-    item.remove();
-
-    // Remove item from storage
-    removeItemFromStorage(item.textContent);
-
-    checkUI();
-  }
-}
-
-function removeItemFromStorage(item) {
-  let itemsFromStorage = getItemsFromStorage();
-
-  // Filter out item to be removed
-  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
-
-  // Re-set to localstorage
-  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
-}
-
-function clearItems() {
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
-  }
-
-  // Clear from localStorage
-  localStorage.removeItem('items');
-
-  checkUI();
-}
-
-function filterItems(e) {
-  const items = itemList.querySelectorAll('li');
-  const text = e.target.value.toLowerCase();
-
-  items.forEach((item) => {
-    const itemName = item.firstChild.textContent.toLowerCase();
-
-    if (itemName.indexOf(text) != -1) {
-      item.style.display = 'flex';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
-
-function checkUI() {
-  itemInput.value = '';
-
-  const items = itemList.querySelectorAll('li');
-
-  if (items.length === 0) {
-    clearBtn.style.display = 'none';
-    itemFilter.style.display = 'none';
-  } else {
-    clearBtn.style.display = 'block';
-    itemFilter.style.display = 'block';
-  }
-
-  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
-  formBtn.style.backgroundColor = '#333';
-
-  isEditMode = false;
-}
-
-// Initialize app
-function init() {
-  // Event Listeners
-  itemForm.addEventListener('submit', onAddItemSubmit);
-  itemList.addEventListener('click', onClickItem);
-  clearBtn.addEventListener('click', clearItems);
-  itemFilter.addEventListener('input', filterItems);
-  document.addEventListener('DOMContentLoaded', displayItems);
-
-  checkUI();
-}
-
-init();
+itemForm.addEventListener("submit", addItem);
